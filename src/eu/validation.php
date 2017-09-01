06d68bdeb061cd99d551abc639bec35e2839c8d0
Andoni Sanchez <sanchezandoni@hotmail.com>
2017-09-02 01:57:43 +0200

    Validation file partially translated

---
+++ src/eu/validation.php
@@ -0,0 +1,131 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'             => ':attribute onartua izan behar da.',
+    'active_url'           => ':attribute ez da baliozko URLa.',
+    //'after'                => 'The :attribute must be a date after :date.',
+    //'after_or_equal'       => 'The :attribute must be a date after or equal to :date.',
+    'alpha'                => ':attribute letrak soilik izan ditzake.',
+    'alpha_dash'           => ':attribute letrak, zenbakiak eta marratxoak soilik izan ditzake.',
+    'alpha_num'            => ':attribute letrak, zenbakiak soilik izan ditzake.',
+    //'array'                => 'The :attribute must be an array.',
+    //'before'               => 'The :attribute must be a date before :date.',
+    //'before_or_equal'      => 'The :attribute must be a date before or equal to :date.',
+    /*
+    'between'              => [
+        'numeric' => 'The :attribute must be between :min and :max.',
+        'file'    => 'The :attribute must be between :min and :max kilobytes.',
+        'string'  => 'The :attribute must be between :min and :max characters.',
+        'array'   => 'The :attribute must have between :min and :max items.',
+    ],
+    */
+    'boolean'              => ':attribute egia edo gezurra izan behar du.',
+    'confirmed'            => ':attribute berrespena ez dator bat.',
+    'date'                 => ':attribute ez da baliozko data.',
+    //'date_format'          => 'The :attribute does not match the format :format.',
+    'different'            => ':attribute eta :other ezberdinak izan behar dira.',
+    /*
+    'digits'               => 'The :attribute must be :digits digits.',
+    'digits_between'       => 'The :attribute must be between :min and :max digits.',
+    'dimensions'           => 'The :attribute has invalid image dimensions.',
+    'distinct'             => 'The :attribute field has a duplicate value.',
+    */
+    'email'                => ':attribute baliozko helbide elektronikoa izan behar du.',
+    'exists'               => 'Hautatutako :atribute baliogabea da',
+    'file'                 => ':attribute fitxategi bat izan behar du.',
+    'filled'               => ':attribute eremua beharrezkoa da.',
+    'image'                => ':attribute irudi bat izan behar du.',
+    /*
+    'in'                   => 'The selected :attribute is invalid.',
+    'in_array'             => 'The :attribute field does not exist in :other.',
+    'integer'              => 'The :attribute must be an integer.',
+    'ip'                   => 'The :attribute must be a valid IP address.',
+    'ipv4'                 => 'The :attribute must be a valid IPv4 address.',
+    'ipv6'                 => 'The :attribute must be a valid IPv6 address.',
+    'json'                 => 'The :attribute must be a valid JSON string.',
+    'max'                  => [
+        'numeric' => 'The :attribute may not be greater than :max.',
+        'file'    => 'The :attribute may not be greater than :max kilobytes.',
+        'string'  => 'The :attribute may not be greater than :max characters.',
+        'array'   => 'The :attribute may not have more than :max items.',
+    ],
+    'mimes'                => 'The :attribute must be a file of type: :values.',
+    'mimetypes'            => 'The :attribute must be a file of type: :values.',
+    'min'                  => [
+        'numeric' => 'The :attribute must be at least :min.',
+        'file'    => 'The :attribute must be at least :min kilobytes.',
+        'string'  => 'The :attribute must be at least :min characters.',
+        'array'   => 'The :attribute must have at least :min items.',
+    ],
+    'not_in'               => 'The selected :attribute is invalid.',
+    */
+    'numeric'              => ':attribute zenbaki bat izan behar du.',
+    /*
+    'present'              => 'The :attribute field must be present.',
+    'regex'                => 'The :attribute format is invalid.',
+    */
+    'required'             => ':attribute eremua nahitaezkoa da.',
+    /*
+    'required_if'          => 'The :attribute field is required when :other is :value.',
+    'required_unless'      => 'The :attribute field is required unless :other is in :values.',
+    'required_with'        => 'The :attribute field is required when :values is present.',
+    'required_with_all'    => 'The :attribute field is required when :values is present.',
+    'required_without'     => 'The :attribute field is required when :values is not present.',
+    'required_without_all' => 'The :attribute field is required when none of :values are present.',
+    'same'                 => 'The :attribute and :other must match.',
+    'size'                 => [
+        'numeric' => 'The :attribute must be :size.',
+        'file'    => 'The :attribute must be :size kilobytes.',
+        'string'  => 'The :attribute must be :size characters.',
+        'array'   => 'The :attribute must contain :size items.',
+    ],
+    'string'               => 'The :attribute must be a string.',
+    'timezone'             => 'The :attribute must be a valid zone.',
+    'unique'               => 'The :attribute has already been taken.',
+    'uploaded'             => 'The :attribute failed to upload.',
+    'url'                  => ':attribute formatua baliogabea da.',
+    /**/
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom'               => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes'           => [],
+
+];
