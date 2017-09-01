9fa77da77499730e063c4ca9ec5010df3eb82a8e
Andoni Sanchez <sanchezandoni@hotmail.com>
2017-09-02 01:53:47 +0200

    Passwords file translated

---
+++ src/eu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Pasahitzak gutxienez sei karaktere izan behar ditu eta elkarrekin bat egin.',
+    'reset'    => 'Pasahitza berrezarri da!',
+    'sent'     => 'Pasahitza berrezartzeko esteka bidali dizugu!',
+    'token'    => 'Pasahitza berrezartzeko kodea baliogabea da.',
+    'user'     => "Ezin izan dugu helbide elektroniko horrekin erabiltzaile bat aurkitu.",
+
+];
